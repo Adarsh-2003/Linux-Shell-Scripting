93043a849acece7f178298c00224638535a03806
Adarsh-2003 <adarshgupta.contact@gmail.com>
2025-04-30 23:44:29 +0530

    wildcards

--- linux4noobs.sh
+++ linux4noobs.sh
@@ -1,3 +1,15 @@
 # creates an Ascii art text file with the word "Hello"
 figlet "Hello" > welcome.txt
 cat welcome.txt
+
+# wildcards
+touch file1.txt file2.txt file3.txt
+ls *.txt # List all .txt files , * is a wildcard
+ls file* # List all files starting with "file"
+
+# Create note_1.txt to note_5.txt
+touch note_{1..5}.txt
+ls note_*.txt # List all note files
+
+# ctrl + L to clear the terminal
+
